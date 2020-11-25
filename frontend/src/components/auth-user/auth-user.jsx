c7a518367eca3121264554c1d077c6f032a6b5a1
poymanov <n.poymanov@gmail.com>
2020-11-25 21:52:41 +0300

    Добавить выход из профиля пользователя (fix #43)

--- frontend/src/components/auth-user/auth-user.jsx
+++ frontend/src/components/auth-user/auth-user.jsx
@@ -1,20 +1,35 @@
-import React from "react";
+import React, {Fragment} from "react";
 import UserTypes from "../../types/users";
 import {currentUserSelector} from "../../store/selectors";
 import {connect} from "react-redux";
+import {flushCurrentUser} from "../../store/action";
+import PropTypes from "prop-types";
+import {Link} from "react-router-dom";
 
-function AuthUser({currentUser}) {
+function AuthUser({currentUser, flushCurrentUser}) {
     return (
-        <span className="navbar-text d-sm-none d-lg-block">{currentUser.name.full}</span>
+        <Fragment>
+            <span className="navbar-text d-sm-none d-lg-block">
+                <Link to="/" className="nav-link"><b>{currentUser.name.full}</b></Link>
+            </span>
+            <span className="navbar-text d-sm-none d-lg-block">
+                <Link to="/" className="nav-link" onClick={flushCurrentUser}>Выход</Link>
+            </span>
+        </Fragment>
     );
 }
 
 AuthUser.propTypes = {
-    currentUser: UserTypes.item
+    currentUser: UserTypes.item,
+    flushCurrentUser: PropTypes.func.isRequired
 }
 
 const mapStateToProps = (state) => ({
     currentUser: currentUserSelector(state)
 });
 
-export default connect(mapStateToProps, null)(AuthUser);
+const mapDispatchToProps = {
+    flushCurrentUser
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(AuthUser);
--- frontend/src/store/reducers/users/users.js
+++ frontend/src/store/reducers/users/users.js
@@ -9,6 +9,7 @@ const users = (state = initialState, action) => {
         case ActionType.LOAD_PROFILE:
             return {...state, ...{currentUser: action.payload}};
         case ActionType.FLUSH_CURRENT_USER:
+            localStorage.removeItem('accessToken');
             return {...state, ...{currentUser: null}};
         default:
             return state;
